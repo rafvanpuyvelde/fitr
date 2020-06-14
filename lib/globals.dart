1bcae16366a7ae3225bdc99597b0fcf3a31cfedf
Raf Vanpuyvelde <raf.vanpuyvelde@outlook.com>
2020-06-14 13:09:47 +0200

    Readme edit

--- lib/globals.dart
+++ lib/globals.dart
@@ -2,7 +2,7 @@ library fitr.globals;
 
 import 'package:flutter/painting.dart';
 
-String baseApiUrl = 'http://b20e50e39a5e.ngrok.io';
+String baseApiUrl = 'http://a6ac326d39bd.ngrok.io';
 
 Color primaryColor = Color.fromARGB(255, 231, 238, 252);
 Color secondaryColor = Color.fromARGB(255, 245, 248, 255);
